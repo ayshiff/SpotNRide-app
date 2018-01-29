0bb9bd3125a9c83c2bdb895464463fd3a5463c73
ayshiff <remi.d45@gmail.com>
2018-01-30 00:11:56 +0100

    Modified App.js

--- SpotNRide/App.js
+++ SpotNRide/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 import { MapView, Location, Permissions, Constants } from 'expo';
 
@@ -9,6 +9,7 @@ state = {
   latitude: null,
   longitude : null,
   errorMessage: null,
+  text: 'Rechercher'
     }
 
   componentWillMount() {
@@ -30,14 +31,16 @@ state = {
        latitude: location.coords.latitude,
        longitude: location.coords.longitude,
       });
-      
+      this.forceUpdate()
   };
 
   render() {
     console.log(this.state.latitude)
     return (
+      <View style={styles.container}>
+     
       <MapView
-        style={{ flex: 1 }}
+        style={ styles.map }
         initialRegion={{
           latitude: this.state.latitude,
           longitude: this.state.longitude,
@@ -45,15 +48,34 @@ state = {
           longitudeDelta: 0.0421,
         }}
       />
+       <TextInput
+      style={styles.search}
+      onChangeText={(text) => this.setState({text})}
+      value={this.state.text}
+      />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 1
   },
+  container:{
+    flex: 1
+  },
+  search:{
+    height: 40,
+    borderWidth: 1,
+    zIndex:2147483647,
+    marginRight: 30,
+    marginLeft: 30,
+    marginTop: 60
+  }
 });
