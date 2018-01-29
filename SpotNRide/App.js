2b6d7b0ccea35828c0267b4b843c21d41b41f225
ayshiff <remi.d45@gmail.com>
2018-01-29 22:35:37 +0100

    Modified App.js

--- SpotNRide/App.js
+++ SpotNRide/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { MapView } from 'expo';
+import { MapView, Location, Permissions, Constants } from 'expo';
 
 export default class App extends React.Component {
+
+state = {
+  latitude: null,
+  longitude : null,
+  errorMessage: null,
+    }
+
+  componentWillMount() {
+
+      this._getLocationAsync();
+
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({
+       latitude: location.coords.latitude,
+       longitude: location.coords.longitude,
+      });
+      
+  };
+
   render() {
+    console.log(this.state.latitude)
     return (
       <MapView
         style={{ flex: 1 }}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: this.state.latitude,
+          longitude: this.state.longitude,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
