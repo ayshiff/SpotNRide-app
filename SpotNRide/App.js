9a710ffad051608d15221171c41bfed8e1dec683
ayshiff <remi.d45@gmail.com>
2018-03-04 16:15:01 +0100

    Remove Firebase auth

--- SpotNRide/App.js
+++ SpotNRide/App.js
@@ -11,12 +11,6 @@ export default class App extends React.Component {
     super(props)
     // Initialize Firebase
     const firebaseConfig = {
-    apiKey: "AIzaSyBGeho9fqv7ZR5Wfkxx_9p4fHnIMGHk_wU",
-    authDomain: "spotnride-74ea5.firebaseapp.com",
-    databaseURL: "https://spotnride-74ea5.firebaseio.com",
-    projectId: "spotnride-74ea5",
-    storageBucket: "spotnride-74ea5.appspot.com",
-    messagingSenderId: "1032015059409"
 };
 firebase.initializeApp(firebaseConfig);
     this.state = {
