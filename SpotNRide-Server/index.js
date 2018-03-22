fa70bb1588f945f9c08ee2f868a10a44f4841743
ayshiff <remi.d45@gmail.com>
2018-03-22 15:00:10 +0100

    Update Cross-origin

--- SpotNRide-Server/index.js
+++ SpotNRide-Server/index.js
@@ -7,6 +7,12 @@ let server = express()
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 server.get('/', function(req, res){
     res.setHeader('Content-Type', 'text/html')
     res.status(200).send('<h1> Home </h1>')
